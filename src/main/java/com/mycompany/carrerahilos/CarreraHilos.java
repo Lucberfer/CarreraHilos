7ce878970b6460796a50e0114eaf040942d19340
Lucas <lucberfer@gmail.com>
2024-11-28 15:52:03 +0100

    ._.

---
+++ src/main/java/com/mycompany/carrerahilos/CarreraHilos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.carrerahilos;
+
+/**
+ *
+ * @author lucas
+ */
+public class CarreraHilos {
+
+    public static void main(String[] args) {
+        //Lloramos fuerte?
+    }
+}
